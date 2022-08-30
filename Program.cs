acf339391adc0187244e12e196793e571dcb506b
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-30 11:20:29 +0300

    first task is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
+убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+using static System.Console;
+Clear();
+
+WriteLine($"Task # 54: Define a two-dimensional array. Write a program to sort in descending order the elements of each row of a two-dimensional array");
+WriteLine($"\nEnter array size m x n and range of random values:");
+int m = InputNumbers("Еnter the number of rows in the array:: ");
+int n = InputNumbers("Еnter the number of columns in the array: ");
+int range = InputNumbers("Enter range: from 1 to ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+WriteLine($"\nSorted array: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Write(input);
+  int output = Convert.ToInt32(ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Write(array[i, j] + " ");
+    }
+    WriteLine();
+  }
+}
