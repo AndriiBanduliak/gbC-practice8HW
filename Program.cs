076ffdfd9bf2a804e65528b8d08aac91d6ceb209
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-30 12:50:04 +0300

    second task is done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
-﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
-убыванию элементы каждой строки двумерного массива.
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
 Например, задан массив:
+
 1 4 7 2
+
 5 9 2 3
+
 8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
 
 using static System.Console;
@@ -22,33 +26,35 @@ int[,] array = new int[m, n];
 CreateArray(array);
 WriteArray(array);
 
-WriteLine($"Sorted array: ");
-OrderArrayLines(array);
-WriteArray(array);
-
-void OrderArrayLines(int[,] array)
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
-        {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
-        }
-      }
-    }
+    sumLine = tempSumLine;
+    minSumLine = i;
   }
+}
+
+WriteLine($"The {minSumLine+1} line with the smallest amount of elements ({sumLine})  ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
 }
 
 int InputNumbers(string input)
 {
   Write(input);
-  int output = Convert.ToInt32(ReadLine());
+  int output = Convert.ToInt32(Console.ReadLine());
   return output;
 }
 
@@ -63,13 +69,13 @@ void CreateArray(int[,] array)
   }
 }
 
-void WriteArray(int[,] array)
+void WriteArray (int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Write(array[i, j] + " ");
+      Write(array[i,j] + " ");
     }
     WriteLine();
   }
